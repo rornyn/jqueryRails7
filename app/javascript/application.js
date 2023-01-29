edf2d2fa104f73f3fc80d85a6c53b3f794665858
Nayan Gupta <nayan.neema93@gmail.com>
2023-01-30 01:55:24 +0530

    jQuery with Rails 7

---
+++ app/javascript/application.js
@@ -0,0 +1,12 @@
+// Configure your import map in config/importmap.rb. Read more: https://github.com/rails/importmap-rails
+import "@hotwired/turbo-rails"
+import "controllers"
+import "jquery"
+import "jquery_ujs"
+
+
+
+$(document).ready(function(){
+ alert($("#test").attr('value'));
+});
+
